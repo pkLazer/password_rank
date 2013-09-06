81f96076e2416e070472baf9c72881aab2e1c4c7
pklazer <skylrroj0714@aol.com>
2013-09-05 21:26:29 -0700

    added checkIfCommon, checkComplexity, and checkLength methods; fixing bugs

--- pwrank.rb
+++ pwrank.rb
@@ -1,24 +1,23 @@
 #!/usr/bin/ruby
-lower = ["a", "b", "c", "d"]
-upper = Array.new
-special = Array.new
-nums = Array.new
-common = Array.new
-# when using regex, need to check that the WHOLE word is is in the list,
-# not just parts. i.e. 
+$lower = /[a-z]/ # matches all lower case  characters
+$upper = /[A-Z]/ # matches all upper case characters
+$special = /\W\s/ # matches special characters, i.e. !@#$%^&*
+$nums = /[0-9]/ # matches all numbers
+$common = Array.new
 
 wordsFile = File.new("./4000-most-common-english-words-csv.csv", "r")
 for word in wordsFile
-	common = common << word
+	$common = $common << word
 end
 wordsFile.close
 
 class ComplexString
 	@string = nil
-	@score = 0
+	@score = nil
 
 	def initialize(password)
 		@string = password
+		@score = 0
 	end
 
 	# Takes a string S and returns an integer 0-3 
@@ -26,9 +25,9 @@ class ComplexString
 		strLen = @string.length
 		if strLen > 8
 			@score += 3
-		elsif strLen > 5 & strLen <= 8
+		elsif strLen > 5 && strLen <= 8
 			@score += 2
-		elsif strLen > 3 & strLen <= 5
+		elsif strLen > 3 && strLen <= 5
 			@score += 1
 		end
 	end
@@ -48,21 +47,67 @@ class ComplexString
 
 	# adds 1 point to SCORE if the ComplexString does not contain a common word
 	def checkIfCommon
-		if !common.include? @string
+		hasMatch = false
+		for word in $common
+			if /#{@string}/ =~ word
+				hasMatch = true
+				break
+			end
+		end
+		puts hasMatch
+		if !hasMatch
+			puts "does not have common"
 			@score += 1
 		end
 	end
 
+	# Increments SCORE by the addinonal levels of complexity, if the passowrd
+	# is complex.
 	def checkComplexity
-		@hasSpecial = false
-		@hasUpper = false
-		@hasLower = false
-		@hasNums = false
+		hasSpecial = false
+		hasUpper = false
+		hasLower = false
+		hasNums = false
+		for word in $common
+			if $lower =~ word && !hasLower
+				@score += 1
+				hasLower = true
+			end
+			#if $upper =~ word && !hasUpper
+			#	@score += 2
+			#	hasUpper = true
+			#end
+			if $nums =~ word && !hasNums
+				@score += 1
+				hasNums = true
+			end
+			#f $special =~ word && !hasSpecial
+			#	@score += 2
+			#	hasSpecial = true
+			#end
+		end
 	end
 end
 
-password = "skyler"
+# main
+if ARGV.length == 1
+	password = ARGV[0]
+else
+	puts "Enter password: "
+	password = gets # if empty, password defaluts to a newline character"\n"
+end
+while (password == "\n")
+	puts "you must enter a password"
+	puts "Enter password: "
+	password = gets
+end
 password = ComplexString.new(password)
 if !password.tooShort?
-	puts password.getString
+	password.checkLen
+	password.checkIfCommon
+	#password.checkComplexity
+	puts password.getScore.to_s
+else
+	puts "your password is too short"
+	exit
 end
